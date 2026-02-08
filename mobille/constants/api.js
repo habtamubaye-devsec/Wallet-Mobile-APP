8655377cb966f60d807ada5636930754a45bf0e8
habtamubaye-devsec <habtamubaye2127@gmail.com>
2026-02-08 17:06:44 +0300

    feat: implement transaction management features with balance display, transaction listing, and deletion functionality

--- mobille/constants/api.js
+++ mobille/constants/api.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://wallet-api-cxqp.onrender.com/api";
+export const API_URL = "https://wallet-mobile-app-6zj8.onrender.com/api";
 // export const API_URL = "http://localhost:5001/api";
