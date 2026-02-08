2dc65849d64399aafc93ca336371b21cfbce84a7
habtamubaye-devsec <habtamubaye2127@gmail.com>
2026-02-08 11:12:47 +0300

    feat: add authentication layout and update dependencies for keyboard handling

---
+++ mobille/app/_layout.tsx
@@ -0,0 +1,12 @@
+import {Slot} from "expo-router";
+import SafeScreen from "../components/SafeScreen";
+import { ClerkProvider } from "@clerk/clerk-expo";
+
+
+export default function RootLayout() {
+  return <ClerkProvider>
+    <SafeScreen>
+      <Slot />
+    </SafeScreen>
+  </ClerkProvider>;
+}
